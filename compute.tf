0081b79110df51ddb642fd472348c8bb66ca9831
efbar <17676301+efbar@users.noreply.github.com>
2021-01-25 18:37:19 +0100

    add cloud init

--- compute.tf
+++ compute.tf
@@ -44,13 +44,174 @@ resource "aws_key_pair" "hashicorp-keypair" {
   public_key = tls_private_key.ssh-key.public_key_openssh
 }
 
+data "cloudinit_config" "agents-configs" {
+  gzip = true
+  base64_encode = true
+  
+  part {
+    content_type = "text/x-shellscript"
+    content = file("${path.module}/scripts/startup-script.sh")
+    filename = "startup-script.sh"
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(templatefile("${path.module}/files/agent.hcl.tpl",
+  {
+    vault_endpoint      = "test:8200"
+    tcp_listener        = "127.0.0.1:8200"
+    tcp_listener_tls    = false
+  }
+))},
+    encoding: b64
+    owner: root:root
+    path: /etc/vault.d/agent.hcl
+    permissions: '0750'
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(templatefile("${path.module}/files/consul-agent.hcl.tmpl",
+  {
+    cluster_nodes = {for n in aws_instance.hashicorp_cluster: n.tags["Name"] => n.private_ip},
+    dc_name       = var.dc_name
+  }
+))},
+    encoding: b64
+    owner: root:root
+    path: /etc/consul.d/consul-agent.hcl
+    permissions: '0750'
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(file("${path.module}/files/ca.tmpl"))},
+    encoding: b64
+    owner: root:root
+    path: /etc/consul.d/ca.tmpl
+    permissions: '0750'
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(file("${path.module}/files/cert.tmpl"))},
+    encoding: b64
+    owner: root:root
+    path: /etc/consul.d/cert.tmpl
+    permissions: '0750'
+EOF
+  }
+  
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(file("${path.module}/files/keyfile.tmpl"))},
+    encoding: b64
+    owner: root:root
+    path: /etc/consul.d/keyfile.tmpl
+    permissions: '0750'
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(templatefile("${path.module}/files/nomad-client.hcl.tmpl",
+  {
+    cluster_nodes = {},
+    dc_name       = var.dc_name
+    envoy_proxy_image = "asfdas"
+  }
+))},
+    encoding: b64
+    owner: root:root
+    path: /etc/nomad.d/nomad-client.hcl
+    permissions: '0750'
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(file("${path.module}/files/nomad-ca.tmpl"))},
+    encoding: b64
+    owner: root:root
+    path: /etc/nomad.d/nomad_ca.tmpl
+    permissions: '0750'
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(file("${path.module}/files/nomad-cert.tmpl"))},
+    encoding: b64
+    owner: root:root
+    path: /etc/nomad.d/nomad_cert.tmpl
+    permissions: '0750'
+EOF
+  }
+  
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+#cloud-config
+write_files:
+  - content: |
+      ${base64encode(file("${path.module}/files/nomad-keyfile.tmpl"))},
+    encoding: b64
+    owner: root:root
+    path: /etc/nomad.d/nomad_keyfile.tmpl
+    permissions: '0750'
+EOF
+  }
+}
+
 resource "aws_instance" "hashicorp_cluster" {
-  count      = var.cluster_instance_count
+  count = var.cluster_instance_count
 
   ami           = data.aws_ami.centos7.id
   instance_type = "t2.micro"
   subnet_id     = module.vpc.public_subnets[count.index]
   key_name      = aws_key_pair.hashicorp-keypair.key_name
+
+  # user_data = data.cloudinit_config.startup-script.rendered
+
+  # network_interface {
+  #   network_interface_id = aws_network_interface.private_ip[count.index].id
+  #   device_index = 0
+  # }
 
   vpc_security_group_ids = [
     aws_security_group.allow_cluster_ssh.id,
@@ -65,6 +226,12 @@ resource "aws_instance" "hashicorp_cluster" {
   iam_instance_profile        = aws_iam_instance_profile.vault-kms-unseal.id
 
   tags = {
-    Name = format("clustnode%.2d", count.index + 1)
+    Name    = format("clustnode%.2d", count.index + 1)
+    Project = var.prefix
   }
 }
+
+# resource "aws_network_interface" "private_ip" {
+#   count = var.cluster_instance_count
+#   subnet_id       = module.vpc.public_subnets[count.index]
+# }
