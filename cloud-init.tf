af868d83701ab17a04c7b8da109d53d07bcf83c0
efbar <17676301+efbar@users.noreply.github.com>
2021-01-26 16:43:21 +0100

    fix var

--- cloud-init.tf
+++ cloud-init.tf
@@ -1,6 +1,9 @@
 module "cloud-init" {
   source         = "git::ssh://git@github.com/bitrockteam/hashicorp-terraform-cloudinit"
   cluster_nodes  = { for n in aws_instance.hashicorp_cluster : n.tags["Name"] => n.private_ip }
-  vault_endpoint = aws_instance.hashicorp_cluster[0].private_ip
+  vault_endpoint = "http://${aws_instance.hashicorp_cluster[0].private_ip}:8200"
   dc_name        = var.dc_name
+  auto_auth_type = "aws"
+  aws_region     = var.region
+  aws_node_role  = "test"
 }
