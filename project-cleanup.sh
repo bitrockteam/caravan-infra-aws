1db3823f4f2b0c79bf8d81bd38908cc6a526c975
efbar <17676301+efbar@users.noreply.github.com>
2021-02-05 09:55:35 +0100

    clean folder and add color

--- project-cleanup.sh
+++ project-cleanup.sh
@@ -7,7 +7,7 @@ PROFILE=$3
 BUCKET_NAME=$NAME-terraform-state
 TABLE_NAME=$NAME-terraform-state-lock
 
-echo "Deleting S3 Bucket $BUCKET_NAME"
+echo -e "\033[32mDeleting S3 Bucket $BUCKET_NAME\033[0m"
 aws s3api put-bucket-versioning \
   --bucket "$BUCKET_NAME" \
   --region "$REGION" \
@@ -23,9 +23,16 @@ aws s3api delete-bucket \
   --region "$REGION" \
   --profile "$PROFILE"
 
-echo "Deleting DynamoDB Table $TABLE_NAME"
+echo -e "\033[32mDeleting DynamoDB Table $TABLE_NAME\033[0m"
 aws dynamodb delete-table \
   --region "$REGION" \
   --table-name "$TABLE_NAME" \
   --profile "$PROFILE"
+
+
+echo -e "\033[32mCleaning folder..\033[0m"
+ls -a | grep -i "root-token" | xargs rm
+ls -a | grep -i "key.json" | xargs rm
+
+echo -e "\033[32mDone!\033[0m"
 
