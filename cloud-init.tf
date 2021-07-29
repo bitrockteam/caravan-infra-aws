19636de9335eae5b91c000f58b8dd21f16d03c97
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-07-29 12:33:52 +0200

    Add partition_prefix

--- cloud-init.tf
+++ cloud-init.tf
@@ -4,13 +4,13 @@ locals {
 }
 
 module "cloud_init_control_plane" {
-  source         = "git::https://github.com/bitrockteam/caravan-cloudinit?ref=feature/mount-data-disk"
-  cluster_nodes  = { for n in aws_instance.hashicorp_cluster : n.tags["Name"] => n.private_ip }
-  vault_endpoint = "http://127.0.0.1:8200"
-  dc_name        = var.dc_name
-  auto_auth_type = "aws"
-  aws_node_role  = local.control_plane_role_name
-
+  source                   = "git::https://github.com/bitrockteam/caravan-cloudinit?ref=feature/mount-data-disk"
+  cluster_nodes            = { for n in aws_instance.hashicorp_cluster : n.tags["Name"] => n.private_ip }
+  vault_endpoint           = "http://127.0.0.1:8200"
+  dc_name                  = var.dc_name
+  auto_auth_type           = "aws"
+  aws_node_role            = local.control_plane_role_name
+  partition_prefix         = "p"
   vault_persistent_device  = "/dev/sdd"
   consul_persistent_device = "/dev/sde"
   nomad_persistent_device  = "/dev/sdf"
