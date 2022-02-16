2ccf2cc85ebedae2cc28c9e3c69ecbd704e2943d
Andrea Simonini <andrea.simonini@gmail.com>
2022-02-16 09:39:20 +0100

    Align minimum terraform version (#39)

--- providers.tf
+++ providers.tf
@@ -1,33 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    acme = {
-      source = "vancluever/acme"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-  }
-}
-
 provider "aws" {
   region                  = var.region
   shared_credentials_file = var.shared_credentials_file
   profile                 = var.awsprofile
-}
-
-terraform {
-  required_version = "~> 0.15.4"
 }
